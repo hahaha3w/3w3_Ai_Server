148111504342c37561467ab3b4323853e3fd5526
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2025-03-21 23:50:54 +0800

    feat: add log package

--- app/chat/pkg/log/log.go
+++ app/chat/pkg/log/log.go
@@ -1,1 +1,15 @@
 package log
+
+import logger "github.com/hahaha3w/3w3_Ai_Server/common/logs"
+
+var log logger.Logger
+
+func RegisterLogger(logger logger.Logger) {
+	log = logger
+}
+func Log() logger.Logger {
+	if log == nil {
+		panic("implement not found for interface Logger, please register")
+	}
+	return log
+}
